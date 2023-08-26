7624dd2601b0e27dd725ddbaa037b5e75c3106d5
Jaswinder Kantiwal <jaswinder.kantiwal@stud.th-deg.de>
2023-08-26 11:18:59 +0200

    created the whole chess game while drinking a cup of coffee

---
+++ chess.py
@@ -0,0 +1,159 @@
+import tkinter as tk
+from tkinter import PhotoImage
+from chess_pieces import King, Queen, Rook, Bishop, Knight, Pawn
+import math
+
+class ChessGame(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.selected_piece = None
+        self.selected_piece_row = None
+        self.selected_piece_col = None
+        self.highlighted_cells = []
+
+        self.title("Chess")
+
+        self.board = [[None for _ in range(8)] for _ in range(8)]
+
+        # Initialize the board with pieces
+        self.board[7][0] = Rook("black")
+        self.board[7][1] = Knight("black")
+        self.board[7][2] = Bishop("black")
+        self.board[7][3] = Queen("black")
+        self.board[7][4] = King("black")
+        self.board[7][5] = Bishop("black")
+        self.board[7][6] = Knight("black")
+        self.board[7][7] = Rook("black")
+
+        self.board[0][0] = Rook("white")
+        self.board[0][1] = Knight("white")
+        self.board[0][2] = Bishop("white")
+        self.board[0][3] = Queen("white")
+        self.board[0][4] = King("white")
+        self.board[0][5] = Bishop("white")
+        self.board[0][6] = Knight("white")
+        self.board[0][7] = Rook("white")
+
+        for col in range(8):
+            self.board[1][col] = Pawn("white")
+            self.board[6][col] = Pawn("black")
+
+        self.canvas = tk.Canvas(self, width=400, height=400)
+        self.canvas.pack()
+
+        self.draw_board()
+        self.draw_pieces()
+
+        self.canvas.bind("<Button-1>", self.on_click)
+
+    def draw_board(self):
+        colors = ["white", "light grey"]
+        for row in range(8):
+            for col in range(8):
+                color_index = (row + col) % 2
+                color = colors[color_index]
+                x0, y0 = col * 50, row * 50
+                x1, y1 = x0 + 50, y0 + 50
+                self.canvas.create_rectangle(x0, y0, x1, y1, fill=color)
+
+    def draw_pieces(self):
+        self.piece_images = {}  # Store PhotoImage instances for each piece
+        piece_images_dir = "./pieces-png"  # Replace with the actual path
+        square_size = 50  # Size of each square
+        for row in range(8):
+            for col in range(8):
+                piece = self.board[row][col]
+                if piece:
+                    image_path = f"{piece_images_dir}/{piece.color}/{piece.symbol.lower()}.png"
+                    self.piece_images[piece] = self.resize_image(image_path)
+                    x, y = col * square_size + square_size // 2, row * square_size + square_size // 2
+                    piece_item = self.canvas.create_image(x, y, image=self.piece_images[piece], tags="piece")
+                    piece.canvas_item = piece_item
+    
+    def resize_image(self, image_path):
+        image = PhotoImage(file=image_path)
+        width = 3
+        height = 3
+        resized_image = image.subsample(width, height)
+        return resized_image
+
+
+    def on_click(self, event):
+        col = event.x // 50
+        row = event.y // 50
+        print(f"Clicked on row {row}, col {col}")
+
+        if self.selected_piece:
+            if (row, col) in self.highlighted_cells:
+                self.move_piece(row, col)
+                self.clear_selection()
+                self.draw_board()
+                self.draw_pieces()
+            else:
+                self.clear_highlight()
+                self.selected_piece = None
+        else:
+            self.clear_highlight()
+            self.selected_piece = self.board[row][col]
+            if self.selected_piece:
+                self.selected_piece_row, self.selected_piece_col = row, col
+                moves = self.selected_piece.valid_moves(self.board, row, col)
+                print(f"Valid moves: {moves}")
+                self.highlighted_cells = moves
+                self.draw_highlight()
+
+
+    def clear_highlight(self):
+        for r, c in self.highlighted_cells:
+            self.canvas.delete("highlight")
+        self.highlighted_cells = []
+
+    def clear_selection(self):
+        self.selected_piece = None
+        self.clear_highlight()
+
+    def move_piece(self, dest_row, dest_col):
+        src_row, src_col = self.selected_piece_row, self.selected_piece_col
+        piece_to_move = self.board[src_row][src_col]
+        
+        # Check for castling move
+        if isinstance(piece_to_move, King) and abs(dest_col - src_col) == 2:
+            if dest_col > src_col:
+                rook_col = 7
+                new_rook_col = dest_col - 1
+            else:
+                rook_col = 0
+                new_rook_col = dest_col + 1
+
+            
+            rook_to_move = self.board[src_row][rook_col]
+            self.board[src_row][new_rook_col] = rook_to_move
+            self.board[src_row][rook_col] = None
+            self.board[src_row][new_rook_col].has_moved = True
+
+        if isinstance(piece_to_move, King):
+            piece_to_move.has_moved = True
+        
+        if isinstance(piece_to_move, Rook):
+            piece_to_move.has_moved = True
+
+
+        # Move the selected piece
+        self.board[dest_row][dest_col] = piece_to_move
+        self.board[src_row][src_col] = None
+        self.clear_highlight()
+
+
+
+    def draw_highlight(self):
+        # self.clear_highlight()
+        for r, c in self.highlighted_cells:
+            x0, y0 = c * 50, r * 50
+            x1, y1 = x0 + 50, y0 + 50
+            self.canvas.create_rectangle(x0, y0, x1, y1, fill="", outline="blue", width=3, tags="highlight")
+
+
+if __name__ == "__main__":
+    game = ChessGame()
+    game.mainloop()
---
+++ chess_pieces.py
@@ -0,0 +1,172 @@
+class Piece:
+    def __init__(self, color):
+        self.color = color
+
+    def __str__(self):
+        return self.symbol
+    
+    def is_valid_move(self, row, col):
+        return 0 <= row < 8 and 0 <= col < 8
+
+    def valid_moves(self, board, row, col):
+        pass
+
+class King(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = "K"
+        self.has_moved = False
+
+    def valid_moves(self, board, row, col):
+        moves = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            if self.is_valid_move(r, c) and (board[r][c] is None or board[r][c].color != self.color):
+                moves.append((r, c))
+
+        # Add castling moves
+        if not self.has_moved and self.color == "black" and board[7][7] and not board[7][7].has_moved:
+            if board[7][5] is None and board[7][6] is None:
+                moves.append((7, 6))
+        if not self.has_moved and self.color == "black" and board[7][0] and not board[7][0].has_moved:
+            if board[7][1] is None and board[7][2] is None and board[7][3] is None:
+                moves.append((7, 2))
+        
+        if not self.has_moved and self.color == "white" and board[0][7] and not board[0][7].has_moved:
+            if board[0][5] is None and board[0][6] is None:
+                moves.append((0, 6))
+        if not self.has_moved and self.color == "white" and board[0][0] and not board[0][0].has_moved:
+            if board[0][1] is None and board[0][2] is None and board[0][3] is None:
+                moves.append((0, 2))
+
+        return moves
+
+
+
+class Queen(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = "Q"
+
+    def valid_moves(self, board, row, col):
+        moves = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            while self.is_valid_move(r, c):
+                if board[r][c] is None:
+                    moves.append((r, c))
+                elif board[r][c].color != self.color:
+                    moves.append((r, c))
+                    break
+                else:
+                    break
+                r, c = r + dr, c + dc
+        return moves
+
+
+class Rook(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = "R"
+        self.has_moved = False
+
+    def valid_moves(self, board, row, col):
+        moves = []
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            while self.is_valid_move(r, c):
+                if board[r][c] is None:
+                    moves.append((r, c))
+                elif board[r][c].color != self.color:
+                    moves.append((r, c))
+                    break
+                else:
+                    break
+                r, c = r + dr, c + dc
+
+        # Add castling moves
+        if not self.has_moved and self.color == "white" and board[7][7] and not board[7][7].has_moved:
+            if all(board[7][col] is None for col in range(5, 7)):
+                moves.append((7, 6))
+        elif not self.has_moved and self.color == "white" and board[7][0] and not board[7][0].has_moved:
+            if all(board[7][col] is None for col in range(1, 4)):
+                moves.append((7, 2))
+
+        if not self.has_moved and self.color == "black" and board[0][7] and not board[0][7].has_moved:
+            if all(board[0][col] is None for col in range(5, 7)):
+                moves.append((0, 6))
+        elif not self.has_moved and self.color == "black" and board[0][0] and not board[0][0].has_moved:
+            if all(board[0][col] is None for col in range(1, 4)):
+                moves.append((0, 2))
+
+        return moves
+
+
+
+class Bishop(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = "B"
+
+    def valid_moves(self, board, row, col):
+        moves = []
+        directions = [(1, 1), (-1, -1), (1, -1), (-1, 1)]
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            while self.is_valid_move(r, c):
+                if board[r][c] is None:
+                    moves.append((r, c))
+                elif board[r][c].color != self.color:
+                    moves.append((r, c))
+                    break
+                else:
+                    break
+                r, c = r + dr, c + dc
+        return moves
+
+
+class Knight(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = "N"
+
+    def valid_moves(self, board, row, col):
+        moves = []
+        knight_moves = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+        for dr, dc in knight_moves:
+            r, c = row + dr, col + dc
+            if self.is_valid_move(r, c) and (board[r][c] is None or board[r][c].color != self.color):
+                moves.append((r, c))
+        return moves
+
+class Pawn(Piece):
+    def __init__(self, color):
+        super().__init__(color)
+        self.symbol = "P"
+
+    def valid_moves(self, board, row, col):
+        moves = []
+
+        # Define direction based on color
+        direction = 1 if self.color == "white" else -1
+
+        # Moving one step forward
+        new_row = row + direction
+        if self.is_valid_move(new_row, col) and board[new_row][col] is None:
+            moves.append((new_row, col))
+
+        # Moving two steps forward from starting position
+        starting_row = 1 if self.color == "white" else 6
+        if row == starting_row and board[new_row][col] is None and board[new_row + direction][col] is None:
+            moves.append((new_row + direction, col))
+
+        # Capturing diagonally
+        for d_col in [-1, 1]:
+            new_col = col + d_col
+            if self.is_valid_move(new_row, new_col) and board[new_row][new_col] and board[new_row][new_col].color != self.color:
+                moves.append((new_row, new_col))
+
+        return moves
